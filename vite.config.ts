0df6ccd5f595e1f52344325f054a0b39fab42b58
Tyler Reagan <tyler.reagan@elembio.com>
2025-10-23 20:40:21 -0700

    shared compatibility for both localhost and github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: "/shola-time-tracker/", // GitHub Pages base path
   build: {
-    outDir: "dist",
+    outDir: "docs",
     assetsDir: "assets",
   },
 });
